08c7bf05364d3788ad0ae8e4b90f7c4abae2e770
Inteli914900ks <intel.i9.14900ks@gmail.com>
2025-08-22 21:57:40 +0200

    Create r15.lua

---
+++ r15.lua
@@ -0,0 +1,120 @@
+local plr = game.Players.LocalPlayer
+local chr = plr.Character or plr.CharacterAdded:Wait()
+
+
+local noDel = {
+	["Body Colors"] = true,
+	Pants = true,
+	Shirt = true,
+	Health = true,
+	Humanoid = true,
+	Animate = true,
+	Head = true,
+	HumanoidRootPart = true,
+	UpperTorso = true,
+	LowerTorso = true,
+	LeftUpperArm = true,
+	LeftLowerArm = true,
+	LeftHand = true,
+	RightUpperArm = true,
+	RightLowerArm = true,
+	RightHand = true,
+	LeftUpperLeg = true,
+	LeftLowerLeg = true,
+	LeftFoot = true,
+	RightUpperLeg = true,
+	RightLowerLeg = true,
+	RightFoot = true
+}
+
+
+for _, p in ipairs(chr:GetChildren()) do
+	if not noDel[p.Name] then
+		p:Destroy()
+	end
+end
+
+
+for _, c in ipairs(chr:GetChildren()) do
+	if noDel[c.Name] and c:IsA("BasePart") then
+		c.Color = Color3.new(204/255, 142/255, 105/255)
+	end
+end
+
+
+local headMesh = chr.Head:FindFirstChildOfClass("SpecialMesh")
+if headMesh then
+	headMesh.MeshType = Enum.MeshType.Head
+	headMesh.Scale = Vector3.new(1.25, 1.25, 1.25)
+end
+
+
+function CreateMeshOnHead(size, meshId, textureId, offset)
+	local character = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+	local head = character:WaitForChild("Head")
+
+	local meshPart = Instance.new("MeshPart")
+	meshPart.Size = size
+	meshPart.MeshId = meshId
+	meshPart.TextureID = textureId
+	meshPart.Anchored = false
+	meshPart.CanCollide = false
+	meshPart.Name = "HeadAttachmentMesh"
+	meshPart.CFrame = head.CFrame * CFrame.new(offset)
+	meshPart.Parent = workspace
+
+	local weld = Instance.new("WeldConstraint")
+	weld.Part0 = meshPart
+	weld.Part1 = head
+	weld.Parent = meshPart
+
+	meshPart.Parent = character
+end
+
+
+CreateMeshOnHead(Vector3.new(1, 1, 1), "rbxassetid://17224953712", "rbxassetid://17224006178", Vector3.new(0.105, 0.27, -0.155))
+CreateMeshOnHead(Vector3.new(1, 1, 1), "rbxassetid://15308551514", "rbxassetid://12824494005", Vector3.new(0.019, -0.734, -0.015))
+CreateMeshOnHead(Vector3.new(1, 1, 1), "rbxassetid://17106624162", "rbxassetid://12824494005", Vector3.new(0.019, -0.678, 0.122))
+CreateMeshOnHead(Vector3.new(1, 1, 1), "rbxassetid://17106624162", "rbxassetid://12824494005", Vector3.new(0.019, -0.678, 0.122))
+CreateMeshOnHead(Vector3.new(1, 1, 1), "rbxassetid://18150356000", "rbxassetid://18265938644", Vector3.new(0, 0, 0))
+CreateMeshOnHead(Vector3.new(1, 1, 1), "rbxassetid://16266462022", "rbxassetid://16247693913", Vector3.new(0, -0.6, -0.25))
+CreateMeshOnHead(Vector3.new(1, 1, 1), "rbxassetid://104217810692089", "rbxassetid://88185972367692", Vector3.new(0, -0.6, 0))
+CreateMeshOnHead(Vector3.new(1, 1, 1), "rbxassetid://16264444783", "rbxassetid://16264444689", Vector3.new(0, -0.6, 0))
+CreateMeshOnHead(Vector3.new(1, 1, 1), "rbxassetid://14786022859", "rbxassetid://14786029100", Vector3.new(0, -0.4, 0))
+
+
+local pants = chr:FindFirstChildOfClass("Pants") or Instance.new("Pants", chr)
+pants.PantsTemplate = "rbxassetid://4921473855"
+
+local shirt = chr:FindFirstChildOfClass("Shirt") or Instance.new("Shirt", chr)
+shirt.ShirtTemplate = "rbxassetid://14341864332"
+
+
+function CreateMeshOnTorso(size, meshId, textureId, offset, rotation)
+	local character = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+	local torso = character:FindFirstChild("UpperTorso")
+	if not torso then return end
+
+	local meshPart = Instance.new("MeshPart")
+	meshPart.Size = size
+	meshPart.MeshId = meshId
+	meshPart.TextureID = textureId
+	meshPart.Anchored = false
+	meshPart.CanCollide = false
+	meshPart.Name = "TorsoAttachmentMesh"
+
+	local rotationCFrame = CFrame.Angles(math.rad(rotation.X), math.rad(rotation.Y), math.rad(rotation.Z))
+	meshPart.CFrame = torso.CFrame * CFrame.new(offset) * rotationCFrame
+	meshPart.Parent = workspace
+
+	local weld = Instance.new("WeldConstraint")
+	weld.Part0 = meshPart
+	weld.Part1 = torso
+	weld.Parent = meshPart
+
+	meshPart.Parent = character
+end
+
+
+CreateMeshOnTorso(Vector3.new(1, 1, 1), "rbxassetid://16738154827", "rbxassetid://16738154903", Vector3.new(0, -1.6, 0), Vector3.new(0, 0, 0))
+CreateMeshOnTorso(Vector3.new(1, 1, 1), "rbxassetid://17358738564", "rbxassetid://17358740726", Vector3.new(0, -0.85, -0.035), Vector3.new(180, 0, 180))
